b8ff9b7f4935a6ef6d2974393b558031c96308e9
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2026-01-02 07:57:52 -0300

    Integration by RestClient

--- src/main/java/br/com/devmarodrigues/moderation_service/api/controller/ModerationController.java
+++ src/main/java/br/com/devmarodrigues/moderation_service/api/controller/ModerationController.java
@@ -3,9 +3,13 @@ package br.com.devmarodrigues.moderation_service.api.controller;
 import br.com.devmarodrigues.moderation_service.api.model.ModerationInput;
 import br.com.devmarodrigues.moderation_service.api.model.ModerationResult;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,6 +18,8 @@ import java.util.regex.Pattern;
 @RestController
 @RequestMapping("/api/moderate")
 public class ModerationController {
+
+    private static Logger logger = LogManager.getLogger(ModerationController.class);
 
     private static final Set<String> FORBIDDEND_WORDS = Set.of("raiva", "xingamento");
     private static final Pattern FORBIDDEN_PATTERN =
@@ -24,6 +30,8 @@ public class ModerationController {
     @ResponseStatus(HttpStatus.CREATED)
     public ModerationResult create(@RequestBody ModerationInput moderationInput) {
         Matcher matcher = FORBIDDEN_PATTERN.matcher(moderationInput.getText());
+
+        logger.info(String.format("SERVICE INVOKED"));
 
         if (matcher.find()) {
             return ModerationResult.builder()
@@ -36,5 +44,4 @@ public class ModerationController {
             .approved(true)
             .build();
     }
-
 }
